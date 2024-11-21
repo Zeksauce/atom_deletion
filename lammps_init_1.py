5e05a01b3cd15ae4be15188c972343d01fa498e3
Zeksauce <zac.jayachandran@gmail.com>
2024-11-21 15:16:46 -0800

    added the compatibility with ovito and lammps for writing and new test files

--- lammps_init_1.py
+++ lammps_init_1.py
@@ -3,14 +3,24 @@ import pandas as pd
 
 
 class AtomDeleter:
-    def __init__(self, path=r"1.2.5 cutoff\1\SrTiO3_0.5_0.data"):
+    def __init__(self, path=r"C:1.2.5 cutoff\1\SrTiO3_0.5_0.data"):
         self.file = Path(path)
-        self.data = pd.read_csv(self.file, delimiter=" ", header= False)
-
-        # Extracting atom data (adjust ranges to your file structure)
-        self.atom_data = self.data.iloc[
-            10 : int(self.data.iloc[0, 0]) + 10, 0:9
-        ].astype(float)
+        self.head = pd.read_csv(self.file, delimiter=" ", nrows=10)
+        df = self.head
+        insert_rows = [9, 6, 5, 2, 0]
+        # Create an empty row as a DataFrame
+        empty_row = pd.DataFrame([[None] * len(df.columns)], columns=df.columns)
+        df = pd.concat([df, empty_row])
+        for index_to_insert in insert_rows:
+            # Split the DataFrame into two parts and concatenate with the empty row
+            df = pd.concat(
+                [df.iloc[:index_to_insert], empty_row, df.iloc[index_to_insert:]]
+            ).reset_index(drop=True)
+        self.head = df
+        data = pd.read_csv(
+            self.file, delimiter=" ", header=10, nrows=int(self.head.iloc[1, 0])
+        ).reset_index()
+        self.atom_data = data
         self.atom_data.columns = [
             "index",
             "Atom",
@@ -19,34 +29,39 @@ class AtomDeleter:
             "y",
             "z",
             "x_velocity",
-            "y_velocity",
+            "y_veloctiy",
             "z_velocity",
         ]
-        self.atom_data.set_index("index", inplace=True)
+        self.atom_data = self.atom_data.drop(columns=["index"])
 
-        # Apply filter conditions
-        self.new = self.atom_data[
+    def main(self):
+        self.remove_atom()
+        self.save()
+
+    def remove_atom(self):
+        atom_data = self.atom_data
+        self.atom_data = atom_data[
             ~(
-                (self.atom_data["x"] > 1.9897)
-                & (self.atom_data["x"] < 195.28)
-                & (self.atom_data["y"] < 1.9551)
-                & (self.atom_data["y"] > 0.25952)
+                (atom_data["x"] > 1.9897)
+                & (atom_data["x"] < 195.3)
+                & (atom_data["y"] < 1.9552)
+                & (atom_data["y"] > 0.25952)
             )
         ]
 
-        # Extract the unchanged header lines
-        header_lines = self.data.iloc[:10]
-
-        # Concatenate header with the updated atom_data
-        self.updated_data = pd.concat([header_lines, self.atom_data], ignore_index=False)
-
-    def save(self):
-        """Save to a new file"""
-        self.updated_data.to_csv(
-            r"1.2.5 cutoff\1\SrTiO3_0.5_00.data", sep=" ", index=False, header=False
+    def save(self, name=r"1.2.5 cutoff\1\SrTiO3_0.5_000.data"):
+        y = self.head.columns[0:9]
+        b = self.atom_data.reset_index(drop=True)
+        b.index = b.index + 1
+        b = b.reset_index()
+        b.columns = y
+        self.head.iloc[1, 0] = len(self.atom_data)
+        x = pd.concat(
+            [self.head, b],
         )
+        x.to_csv(name, sep=" ", index=False, header=True)
 
 
 if __name__ == "__main__":
     deleter = AtomDeleter()
-    deleter.save()
+    deleter.main()
