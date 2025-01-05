aaaa3a2226eccb3c5180fd5a150050526f3de29e
WhatIsLuxray <ravenchan.diaz@gmail.com>
2025-01-04 18:32:17 -0800

    Updated degrees 6, 10, 22, 36, 45 to have bigger lattices and ran init and atom_deleter for them

--- new_atom_deletion.py
+++ new_atom_deletion.py
@@ -17,7 +17,37 @@ degree_4 = (
     4,
 )
 
-run = [degree_1]
+degree_6 = (
+    r"1.2.5 cutoff\6\SrTiO3_0.5_0.data",
+    r"1.2.5 cutoff\6\SrTiO3_0.5_0.txt",
+    6,
+)
+
+degree_10 = (
+    r"1.2.5 cutoff\10\SrTiO3_0.5_0.data",
+    r"1.2.5 cutoff\10\SrTiO3_0.5_0.txt",
+    10,
+)
+
+degree_22 = (
+    r"1.2.5 cutoff\22\SrTiO3_0.5_0.data",
+    r"1.2.5 cutoff\22\SrTiO3_0.5_0.txt",
+    22,
+)
+
+degree_36 = (
+    r"1.2.5 cutoff\36\SrTiO3_0.5_0.data",
+    r"1.2.5 cutoff\36\SrTiO3_0.5_0.txt",
+    45,
+)
+
+degree_45 = (
+    r"1.2.5 cutoff\45\SrTiO3_0.5_0.data",
+    r"1.2.5 cutoff\45\SrTiO3_0.5_0.txt",
+    45,
+)
+
+run = [degree_6, degree_10, degree_22, degree_36, degree_45]
 
 if __name__ == "__main__":
     for degree in run:
